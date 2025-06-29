d541fbdc5a03cd5e0ace8577e472e810747c30f4
Heritier Ganza Tamba <heritierganzatamba@gmail.com>
2025-06-29 17:22:38 +0200

    routes endpoints initiated

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Authcontroller;
+use App\Http\Controllers\PartController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\VehicleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +20,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/login', [Authcontroller::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/vehicles', [VehicleController::class, 'index']);
+    Route::post('/vehicles', [VehicleController::class, 'store']);
+    Route::get('/services', [ServiceController::class, 'index']);
+    Route::post('/services', [ServiceController::class, 'store']);
+    Route::put('/services/{id}', [ServiceController::class, 'update']);
+    Route::get('/parts', [PartController::class, 'index']);
+    Route::post('/parts', [PartController::class, 'store']);
+    Route::put('/parts/{id}', [PartController::class, 'update']);
+    Route::delete('/parts/{id}', [PartController::class, 'destroy']);
 });
